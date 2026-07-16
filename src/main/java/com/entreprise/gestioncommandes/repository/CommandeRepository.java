67d07322f55300dd49767730e1ddba9dc95e8b30
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2026-07-16 12:52:43 +0100

    feat: requetes de comptage et de chiffre d'affaires pour les statistiques

--- src/main/java/com/entreprise/gestioncommandes/repository/CommandeRepository.java
+++ src/main/java/com/entreprise/gestioncommandes/repository/CommandeRepository.java
@@ -2,7 +2,9 @@ package com.entreprise.gestioncommandes.repository;
 
 import com.entreprise.gestioncommandes.model.Commande;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,4 +17,9 @@ public interface CommandeRepository extends JpaRepository<Commande, Long> {
     List<Commande> findByAnnuleeOrderByDateCreationDesc(boolean annulee);
 
     List<Commande> findByDateCreationBetweenOrderByDateCreationDesc(LocalDateTime debut, LocalDateTime fin);
+
+    long countByAnnulee(boolean annulee);
+
+    @Query("select coalesce(sum(c.montantTotalTtc), 0) from Commande c where c.annulee = false")
+    BigDecimal calculerChiffreAffairesActif();
 }
