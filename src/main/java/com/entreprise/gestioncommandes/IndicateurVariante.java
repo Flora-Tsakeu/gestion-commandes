0c24e06d766d28a9b69cf29b829cd7c7fbd72230
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2026-07-28 21:15:32 +0100

    chore: run 0878

--- src/main/java/com/entreprise/gestioncommandes/IndicateurVariante.java
+++ src/main/java/com/entreprise/gestioncommandes/IndicateurVariante.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class IndicateurVariante implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(IndicateurVariante.class);
-    private static final String VARIANTE = "run-877";
+    private static final String VARIANTE = "run-878";
 
     @Override
     public void run(String... args) {
