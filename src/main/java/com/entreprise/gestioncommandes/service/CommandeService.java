f6550c60e158009d29e20f0fd8af055683380885
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2026-08-05 09:25:03 +0100

    [FIX:F1] delais maximum d'annulation passe de 30 à 1

--- src/main/java/com/entreprise/gestioncommandes/service/CommandeService.java
+++ src/main/java/com/entreprise/gestioncommandes/service/CommandeService.java
@@ -36,7 +36,7 @@ import com.entreprise.gestioncommandes.repository.ProduitRepository;
 public class CommandeService {
 
     private static final Logger log = LoggerFactory.getLogger(CommandeService.class);
-    private static final int DELAI_MAX_ANNULATION_JOURS = 1;
+    private static final int DELAI_MAX_ANNULATION_JOURS = 30;
     private static final BigDecimal MONTANT_MINIMUM_HT = new BigDecimal("5.00");
     
     private final CommandeRepository commandeRepository;
