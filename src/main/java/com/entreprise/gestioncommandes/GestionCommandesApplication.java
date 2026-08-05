413545366981c88ed0218014add336e54bf0b3f0
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2026-08-05 14:25:56 +0100

    [FAULT:F2] postconstruct qui lève une exception

--- src/main/java/com/entreprise/gestioncommandes/GestionCommandesApplication.java
+++ src/main/java/com/entreprise/gestioncommandes/GestionCommandesApplication.java
@@ -3,10 +3,18 @@ package com.entreprise.gestioncommandes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class GestionCommandesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GestionCommandesApplication.class, args);
+    }
+
+    @PostConstruct
+    public void initialisationBloquante() {
+        // Cette ligne va s'exécuter au démarrage et faire planter l'application exprès
+        throw new RuntimeException("Échec volontaire provoqué par @PostConstruct pour test");
     }
 }
