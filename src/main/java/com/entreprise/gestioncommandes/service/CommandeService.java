70a9a78312892435dbc61d75b75c43a2290b66b8
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2026-08-05 11:44:06 +0100

    [FIX:F2] bean circulaire , commande service injecte ExportProduitService

--- src/main/java/com/entreprise/gestioncommandes/service/CommandeService.java
+++ src/main/java/com/entreprise/gestioncommandes/service/CommandeService.java
@@ -42,14 +42,12 @@ public class CommandeService {
     private final CommandeRepository commandeRepository;
     private final ProduitRepository produitRepository;
     private final NotificationService notificationService;
-    private final ExportCommandeService exportCommandeService;
 
     public CommandeService(CommandeRepository commandeRepository, ProduitRepository produitRepository,
-                            NotificationService notificationService, ExportCommandeService exportCommandeService) {
+                            NotificationService notificationService) {
         this.commandeRepository = commandeRepository;
         this.produitRepository = produitRepository;
         this.notificationService = notificationService;
-        this.exportCommandeService= exportCommandeService;
     }
 
     @Transactional
