5ff0f7bf8fd8464ad99e1d2ffb0fca59b7c64ce7
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2026-08-05 14:21:02 +0100

    [FIX:F2] propriete alerte;seuil.global non definie

--- src/main/java/com/entreprise/gestioncommandes/service/ProduitService.java
+++ src/main/java/com/entreprise/gestioncommandes/service/ProduitService.java
@@ -11,7 +11,6 @@ import com.entreprise.gestioncommandes.repository.LigneCommandeRepository;
 import com.entreprise.gestioncommandes.repository.ProduitRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -21,9 +20,6 @@ import java.util.List;
 
 @Service
 public class ProduitService {
-
-    @Value("${alerte.seuil.global}")
-    private String seuilGlobal;
 
     private static final Logger log = LoggerFactory.getLogger(ProduitService.class);
 
