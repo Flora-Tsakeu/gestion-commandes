96dcc4688035ab998709cd6ee6b0b00fff4cfab2
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2026-08-19 15:52:00 +0100

    test de communicatioon et de fonctionnement de mon service fast api

--- src/main/java/com/entreprise/gestioncommandes/model/Produit.java
+++ src/main/java/com/entreprise/gestioncommandes/model/Produit.java
@@ -122,4 +122,5 @@ public class Produit {
     public void setActif(boolean actif) {
         this.actif = actif;
     }
+    // J'ecris ceci pour le push de test de communication  avec mon service fast api  et du fonctionnement correct de ce dernier.
 }
